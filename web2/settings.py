d262b23ba21b60167c2d2841672db31e53e6123b
chander kadari <kadarichander@gmail.com>
2025-12-03 13:13:14 +0530

    add template

--- web2/settings.py
+++ web2/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'web2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "myapp" / "templates"],
+        'DIRS': [BASE_DIR /  "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
